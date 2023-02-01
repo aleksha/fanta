96a64686a5b1e2368a8d1ff2c7b3e622d04bf035
Aleksei Dziuba <adzyuba@cern.ch>
2023-02-01 09:28:19 +0300

    offence EPA data

---
+++ off.py
@@ -0,0 +1,41 @@
+from parse_off import stats
+from statistics import mean, stdev
+
+hm = ROOT.TH1F("hm","hm",18,-0.3,+0.3)
+hp = ROOT.TH1F("hp","hp",18,-0.3,+0.3)
+
+best = []
+for pl in stats:
+    if pl["EPApp"]>0.1:
+        best.append(pl)
+
+
+reg = []
+for bs in best:
+    for pl in stats:
+        if pl["team"]==bs["team"] and pl["season"]-1==bs["season"]:
+            #print(pl)
+            #print(bs)
+            #print("  ------- ")
+            reg.append(pl["EPApp"]-bs["EPApp"])
+            hp.Fill(pl["EPApp"]-bs["EPApp"])
+
+
+reg2 = []
+for bs in best:
+    for pl in stats:
+        if pl["team"]==bs["team"] and pl["season"]+1==bs["season"]:
+            reg2.append(bs["EPApp"]-pl["EPApp"])
+            hm.Fill(bs["EPApp"]-pl["EPApp"])
+            if (bs["EPApp"]-pl["EPApp"])>0:
+                print(pl)
+                print(bs)
+                print("  ======== ")
+
+
+hm.red()
+
+hp.Draw()
+hm.Draw("same")
+
+
---
+++ off3.py
@@ -0,0 +1,23 @@
+from parse_off import stats
+from statistics import mean, stdev
+
+h2 = ROOT.TH2F("hm","hm",18,-0.3,+0.3,18,-0.3,+0.3)
+
+best = []
+for pl in stats:
+    if pl["season"]>2010 or pl["season"]<2022:
+        best.append(pl)
+
+
+reg = []
+for bs in best:
+    for pl in stats:
+        if pl["team"]==bs["team"]:
+            if pl["season"]+1==bs["season"]:
+                h2.Fill( bs["EPApp"], pl["EPApp"]-bs["EPApp"])
+
+
+h2.Draw()
+print( "Correlation: " + str( h2.xycorr() ) )
+
+
